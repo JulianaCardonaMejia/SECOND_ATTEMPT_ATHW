73888ddb765db5373cc4d7897eafc8873e244a7a
JulianaCardonaMejia <133095917+JulianaCardonaMejia@users.noreply.github.com>
2023-06-20 20:15:01 +0200

    Add files via upload
    console

---
+++ consoleApp.processNumbers.js
@@ -0,0 +1,11 @@
+// READ THE FILE helloWorld.js AND EXTRACT THE FUNCTION IN A VARIABLE
+const processNumbersFunction = require("./processNumbers.js");
+// CALL/INVOKE THE FUNCTION
+
+const obj = {
+    numbers: [1, 2, 3],
+};
+const response = processNumbersFunction (obj);
+// LOG THE RESPONSE IN MY CONSOLE
+const text = JSON.stringify(response);
+console.log(response);
